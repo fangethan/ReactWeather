079b3e832348d2efd42882cca37e11c1952fff1a
Ethan Fang <ethan.fang@gmail.com>
2023-02-25 14:41:26 +1100

    Add props to the errormodal

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,16 +1,28 @@
 var React = require("react");
+var PropTypes = require('prop-types');
 
 var ErrorModal = React.createClass({
+    getDefaultProps: function () {
+        return {
+            title: "Error",
+        }
+    },
+    propTypes: {
+        title: PropTypes.string,
+        message: PropTypes.string.isRequired,
+    },
     componentDidMount: function () {
         var modal = new Foundation.Reveal($("#error-modal"));
         modal.open();  
     },
 
     render() {
+        var {title, message} = this.props;
+
         return (
             <div id="error-modal" className="reveal tiny text-center" data-reveal="">
-                <h4>Some Title</h4>
-                <p>Our error message</p>
+                <h4>{this.props.title}</h4>
+                <p>{this.props.message}</p>
                 <p>
                     <button className="button hollow" data-close="">Ok</button>
                 </p>
