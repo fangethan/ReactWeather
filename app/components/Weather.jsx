b67f60ef62ca5d445d3177d3d2f7128b841fe133
Ethan Fang <ethan.fang@gmail.com>
2023-02-25 14:29:39 +1100

    Create errormodal component

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -2,6 +2,7 @@ var React = require("react");
 var WeatherForm = require("WeatherForm");
 var WeatherMessage = require("WeatherMessage");
 var openWeatherMap = require("openWeatherMap");
+var ErrorModal = require("ErrorModal");
 
 var Weather = React.createClass({
     getInitialState: function () {
@@ -12,7 +13,10 @@ var Weather = React.createClass({
 
     handleSearch: function (location) {
         var that = this;
-        this.setState({isLoading: true})
+        this.setState({
+            isLoading: true,
+            errorMessage: undefined,
+        })
         
         openWeatherMap.getTemperature(location).then(function (temp) {
             that.setState({
@@ -20,14 +24,16 @@ var Weather = React.createClass({
                 temp: temp,
                 isLoading: false,
             })
-        }, function (errorMessage) {
-            that.setState({isLoading: false})
-            alert(errorMessage)
+        }, function (e) {
+            that.setState({
+                isLoading: false,
+                errorMessage: e.message,
+            })
         })
     },
 
     render() {
-        var {isLoading, location, temp} = this.state;
+        var {isLoading, location, temp, errorMessage} = this.state;
 
         function renderMessage() {
             if (isLoading) {
@@ -37,11 +43,20 @@ var Weather = React.createClass({
             }
         }
 
+        function renderError() {
+            if(typeof errorMessage === "string") {
+                return (
+                    <ErrorModal/>
+                )
+            }
+        }
+
         return (
             <div>
                 <h2 className="text-center">Get Weather</h2>     
                 <WeatherForm onSearch={this.handleSearch}></WeatherForm>
                 {renderMessage()}
+                {renderError()}
             </div>
         );
     }
